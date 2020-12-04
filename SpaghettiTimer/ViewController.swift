0b62945fb5c9251ff1a353616711c7127e48f505
Danielle Lapensee-Rankine <lapenseerankinedl@appstate.edu>
2020-12-03 21:50:02 -0500

    Initial Commit

---
+++ SpaghettiTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaghettiTimer
+//
+//  Created by user180266 on 12/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
