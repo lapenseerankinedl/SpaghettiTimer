822e4b1623449da503f653f8a287eace475f7a0b
Danielle Lapensee-Rankine <lapenseerankinedl@appstate.edu>
2020-12-04 23:31:06 -0500

    end of work session, got most of the buttons working

--- SpaghettiTimer/TimerViewController.swift
+++ SpaghettiTimer/TimerViewController.swift
@@ -11,8 +11,21 @@ class TimerViewController: UIViewController {
 
     @IBOutlet weak var labelDisplay: UILabel!
     @IBOutlet weak var myDatePicker: UIDatePicker!
+    @IBOutlet weak var workButton: UIButton!
+    @IBOutlet weak var sBreakButton: UIButton!
+    @IBOutlet weak var lBreakButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    var resetTime = 0
+    var timer = Timer()
     let dateFormatter: DateFormatter = DateFormatter()
     var receivedString = ""
+    var buttonPressed = ""
+    var currentTime = 0
+    var hitOne = false
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,6 +35,101 @@ class TimerViewController: UIViewController {
         dateFormatter.timeStyle = .short
     }
     
+    @IBAction func workButtonPressed(_ sender: UIButton) {
+        if (!timer.isValid) {
+            workButton.tintColor = UIColor.white
+            workButton.backgroundColor = UIColor.blue
+            sBreakButton.tintColor = UIColor.blue
+            sBreakButton.backgroundColor = UIColor.white
+            lBreakButton.tintColor = UIColor.blue
+            lBreakButton.backgroundColor = UIColor.white
+            buttonPressed = "wButton"
+        }
+            
+    }
     
-
+    @IBAction func sBreakButtonPressed(_ sender: UIButton) {
+        if (!timer.isValid) {
+            sBreakButton.tintColor = UIColor.white
+            sBreakButton.backgroundColor = UIColor.blue
+            workButton.tintColor = UIColor.blue
+            workButton.backgroundColor = UIColor.white
+            lBreakButton.tintColor = UIColor.blue
+            lBreakButton.backgroundColor = UIColor.white
+            buttonPressed = "sButton"
+        }
+        
+    }
+    
+    @IBAction func lBreakButtonPressed(_ sender: UIButton) {
+        if (!timer.isValid) {
+            lBreakButton.tintColor = UIColor.white
+            lBreakButton.backgroundColor = UIColor.blue
+            sBreakButton.tintColor = UIColor.blue
+            sBreakButton.backgroundColor = UIColor.white
+            workButton.tintColor = UIColor.blue
+            workButton.backgroundColor = UIColor.white
+            buttonPressed = "lButton"
+        }
+        
+    }
+    
+    
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        if (buttonPressed == "") {
+            showAlert(messageText: "Please choose a timer option: Work, Short Break, Long Break")
+        }
+        else {
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TimerViewController.updateTimer), userInfo: nil, repeats: true)
+        }
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: UIButton) {
+        timer.invalidate()
+        // TODO:
+        // when timer is stopped
+        // save current left time
+        // and do not print to screen
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        // TODO:
+        // do not save time
+        // possibly delete the saved leftover time when the timer was stopper
+        timer.invalidate()
+        myDatePicker.countDownDuration = TimeInterval(resetTime)
+        
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+    }
+    
+    @objc func updateTimer() {
+        myDatePicker.countDownDuration -= 1
+        if (hitOne == true) {
+            timer.invalidate()
+            // TODO:
+            // add alert here and/or sound
+            hitOne = false
+            showAlert(messageText: "Timer has finished")
+            // save the time when it runs out
+            // and print it to the screen
+            
+            
+        }
+        if (myDatePicker.countDownDuration == 60) {
+            hitOne = true
+        }
+    }
+    
+    func showAlert(messageText: String) {
+        let alert = UIAlertController(title: "Timer", message: "\(messageText)", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: {action -> Void in
+            
+        })
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 }
